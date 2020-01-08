c0b56f8ff4004a8df2678e55e53a4349e9b8dd15
Endel Dreyer <endel.dreyer@gmail.com>
2020-01-07 22:56:05 -0300

    use codefund sticky ads

--- docs/javascripts/ads.js
+++ docs/javascripts/ads.js
@@ -6,11 +6,12 @@ var insertBefore = document.querySelector(".md-content > .md-content__inner");
  */
 var codefundAd = document.createElement("div");
 codefundAd.id = "codefund_ad";
+codefundAd.setAttribute("data-behavior", "sticky");
 content.insertBefore(codefundAd, insertBefore);
 
 var embedScript = document.createElement('script');
 embedScript.type = 'text/javascript';
-embedScript.src = "https://codefund.io/scripts/b7284271-5f29-43a7-8e6e-cd68f0769509/embed.js";
+embedScript.src = "https://codefund.io/properties/b7284271-5f29-43a7-8e6e-cd68f0769509/funder.js";
 document.getElementsByTagName('head')[0].appendChild(embedScript);
 
 /**
