b298a38e57a1697c7d705a507692d385450f6edc
Endel Dreyer <endel.dreyer@gmail.com>
2024-11-26 23:45:10 -0300

    reworking docs with nextra + nextra-docs-template

---
+++ nextra-template/pages/_meta.ts
@@ -0,0 +1,37 @@
+export default {
+    index: {
+        title: "Introduction",
+        theme: {
+            layout: "full"
+        }
+    },
+
+    "getting-started": "Getting Started",
+    "concepts": "Guides",
+    "authentication": "Authentication",
+    "database": "Database",
+
+    documentation: {
+        title: "Documentation",
+        type: "page",
+        href: "/"
+    },
+
+    community: {
+        title: "Community",
+        type: "page",
+        href: "/community"
+    },
+
+    cloud: {
+        title: "Cloud Hosting",
+        type: "page",
+        href: "/cloud"
+    },
+
+    sponsors: {
+        title: "Sponsors",
+        type: "page",
+    },
+
+}
---
+++ nextra-template/theme.config.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import Image from 'next/image'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+
+const logo = <Image src={require('./images/logo.svg')} width={0} height={0} style={{ width: 'auto', height: '28px' }} alt="Colyseus" />;
+
+const config: DocsThemeConfig = {
+    logo,
+    project: {
+        link: 'https://github.com/colyseus/colyseus',
+    },
+    chat: {
+        link: 'http://chat.colyseus.io/',
+    },
+    docsRepositoryBase: 'https://github.com/colyseus/docs',
+    footer: {
+        content: '2024 © Endel Dreyer',
+    },
+    head: function useHead() {
+        const config = useConfig()
+        const title = `${config.title} – Colyseus`
+        const description =
+            config.frontMatter.description || 'Colyseus: Multiplayer Framework for Node.js'
+
+        const image = config.frontMatter.image || 'https://colyseus.io/images/fb-share.png'
+
+        return (
+            <>
+                <title>{title}</title>
+                <meta property="og:title" content={title} />
+                <meta name="description" content={description} />
+                <meta property="og:description" content={description} />
+                <meta name="og:image" content={image} />
+
+                <meta name="msapplication-TileColor" content="#fff" />
+                <meta httpEquiv="Content-Language" content="en" />
+                <meta name="apple-mobile-web-app-title" content="Colyseus" />
+                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="https://nextra.site" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-icon-180x180.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/android-icon-192x192.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="96x96"
+                    href="/favicon-96x96.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+            </>
+        )
+    }
+}
+
+export default config
