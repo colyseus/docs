47065890a6767a477a0b937c2eb9ac5aff41abf2
Endel Dreyer <endel.dreyer@gmail.com>
2025-02-17 12:03:11 -0300

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
 import '../style.css';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />
+  return <>
+    <GoogleAnalytics trackPageViews />
+    <Component {...pageProps} />
+  </>
 }
