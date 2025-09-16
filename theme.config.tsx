70cd56cb477a6fe5d1afe9ce567a4b16bbfe9b31
Robin Nater <76495150+Calfur@users.noreply.github.com>
2025-09-16 16:58:24 +0800

    Add missing favicons

--- theme.config.tsx
+++ theme.config.tsx
@@ -57,19 +57,18 @@ const config: DocsThemeConfig = {
                 <meta property="og:description" content={description} />
                 <meta name="og:image" content={image} />
 
-                <meta name="msapplication-TileColor" content="#fff" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta httpEquiv="Content-Language" content="en" />
                 <meta name="apple-mobile-web-app-title" content="Colyseus" />
-                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+                <meta name="msapplication-TileImage" content="/icon1.png" />
 
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:site" content="https://docs.colyseus.io" />
 
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-                <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="apple-touch-icon" href="/apple-icon.png" />
+                <link rel="icon" type="image/svg+xml" href="/icon0.svg" />
+                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+                <link rel="manifest" href="/manifest.json" />
             </>
         )
     }
