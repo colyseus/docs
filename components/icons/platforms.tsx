45a186776279a12ff1a991742dec2ec0bd085661
Endel Dreyer <endel.dreyer@gmail.com>
2026-02-06 11:13:11 -0300

    fix platform icons on light mode

--- components/icons/platforms.tsx
+++ components/icons/platforms.tsx
@@ -6,51 +6,61 @@ interface IconProps {
 }
 
 export const javascript = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/javascript.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="JavaScript" />
 
 export const typescript = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/typescript.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="TypeScript" />
 
 export const react = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/react.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="React" />
 
 export const unity = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/unity.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="Unity" />
 
 export const defold = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/defold.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="Defold Engine" />
 
 export const construct3 = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/construct3.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="Construct 3" />
 
 export const cocos = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/cocos.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="Cocos Creator" />
 
 export const haxe = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/haxe.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="Haxe" />
 
 export const discord = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/discord.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="Discord" />
 
 export const wechat = (props: IconProps = {}) => <Image
+            className="platform-icon"
             src={require('../../images/icons/wechat.png')}
             width={0} height={0} style={{ width: props.width || '32px', height: 'auto', display: 'inline-block', marginRight: props.marginRight || '0px' }}
             alt="WeChat" />
