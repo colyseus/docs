c924f936436ba9d9bdf0385c7706ad2587666514
Endel Dreyer <endel.dreyer@gmail.com>
2026-03-13 17:00:54 -0300

    gamemaker: ordering. add godot mono disclaimer

--- pages/getting-started/_meta.tsx
+++ pages/getting-started/_meta.tsx
@@ -8,9 +8,9 @@ export default {
     "godot": { title: <span>{godot({ width: '19px', marginRight: '2px' })} Godot</span> },
     "defold": { title: <span>{defold({ width: '19px', marginRight: '2px' })} Defold</span> },
     "construct3": { title: <span>{construct3({ width: '19px', marginRight: '2px' })} Construct 3</span> },
+    "gamemaker": { title: <span>{gamemaker({ width: '19px', marginRight: '2px' })} GameMaker</span> },
     "cocos": { title: <span>{cocos({ width: '19px', marginRight: '2px' })} Cocos Creator</span> },
     "haxe": { title: <span>{haxe({ width: '19px', marginRight: '2px' })} Haxe</span> },
-    "gamemaker": { title: <span>{gamemaker({ width: '19px', marginRight: '2px' })} GameMaker</span> },
     "discord-activity": { title: <span>{discord({ width: '19px', marginRight: '2px' })} Discord Activity</span> },
     "wechat": { title: <span>{wechat({ width: '19px', marginRight: '2px' })} WeChat</span> },
 }
