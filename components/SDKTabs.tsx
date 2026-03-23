a7a35b330595dc73935afcd13fa95a3031d7c0a6
Endel Dreyer <endel.dreyer@gmail.com>
2026-03-23 11:05:32 -0300

    add MonoGame getting started page, platform icon, and Godot Mono NuGet instructions

--- components/SDKTabs.tsx
+++ components/SDKTabs.tsx
@@ -1,7 +1,7 @@
 import { Tabs } from 'nextra/components'
 
 export function SDKTabs({ children }: { children: React.ReactNode }) {
-    return <Tabs items={["TypeScript", "Unity (C#)", "Defold (Lua)", "Haxe", "Godot (GDScript)"]} storageKey='sdk-tabs'>
+    return <Tabs items={["TypeScript", "Unity/Mono (C#)", "Defold (Lua)", "Haxe", "Godot (GDScript)"]} storageKey='sdk-tabs'>
         {children}
     </Tabs>
 }
--- pages/getting-started/_meta.tsx
+++ pages/getting-started/_meta.tsx
@@ -1,9 +1,10 @@
-import { javascript, typescript, react, unity, defold, construct3, cocos, haxe, discord, wechat, gamemaker, godot } from '../../components/icons/platforms'
+import { javascript, typescript, react, unity, defold, construct3, cocos, haxe, discord, wechat, gamemaker, godot, monogame } from '../../components/icons/platforms'
 
 export default {
     "typescript": { title: <span>{typescript({ width: '19px', marginRight: '2px' })} TypeScript</span> },
     "react": { title: <span>{react({ width: '19px', marginRight: '2px' })} React</span> },
     "unity": { title: <span>{unity({ width: '19px', marginRight: '2px' })} Unity</span> },
+    "monogame": { title: <span>{monogame({ width: '19px', marginRight: '2px' })} MonoGame</span> },
     "godot": { title: <span>{godot({ width: '19px', marginRight: '2px' })} Godot</span> },
     "defold": { title: <span>{defold({ width: '19px', marginRight: '2px' })} Defold</span> },
     "construct3": { title: <span>{construct3({ width: '19px', marginRight: '2px' })} Construct 3</span> },
